5b7c882994491d53e9d4e244e4fb2d8c4ce2414b
julzajules <juliamuiruri1999@gmail.com>
2019-10-26 13:33:09 +0300

    I have editted the front page

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static int limit = 0;
+       public static void Main()
+        {
+            /*  int i=0; //declare and initialize a variable
+              for (i=1;i<=100;i++)
+              {
+                  if(i%3!=0 && i%5!=0)
+                  {
+                      WriteLine(i);
+                  }
+                  if(i%3==0)
+                  {
+                      WriteLine("Fizz");
+                  }
+                  if(i%5==0)
+                  {
+                      WriteLine("Buzz");
+                  }
+                  if(i%5==0 && i%3==0)
+                  {
+                      WriteLine("FizzBuzz");
+                  }
+              }
+              ReadKey();*/
+            WriteLine("Multiples of 3= Fizz");
+            WriteLine("Multiples of 5= Buzz");
+            WriteLine("Multiples of both 3 and 5= FizzBuzz");
+            WriteLine("Enter the wanted limit of fizz, buzz and fizzbuzz");
+            limit = int.Parse(ReadLine());
+        }
+
+        private static void fibuzz()
+        {
+            int h;
+            for (h = 1; h <= limit; h++)
+            {
+                if (h % 3 == 0)
+                    WriteLine("..Fizz");
+                if (h % 5 == 0)
+                    WriteLine("..Buzz");
+                if (h % 3 != 0 && h % 5 != 0)
+                    WriteLine(h);
+                if (h % 3 == 0 && h % 5 == 0)
+                    WriteLine("..FizzBuzz \n");
+                ReadKey();
+            }
+        }
+
+        private class lim
+        {
+        }
+    }
+}
